2ab15febbafc50207c1c2b1a3a8c7908d8dc6884
Sergio <sergiodanilomosqueraquigua@gmail.com>
2025-09-19 17:28:03 -0500

    Agregar archivos de configuración y estructura inicial del proyecto Spring Boot

---
+++ src/main/java/com/udlaverso/metaudla/MetaudlaApplication.java
@@ -0,0 +1,13 @@
+package com.udlaverso.metaudla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaudlaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetaudlaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udlaverso/metaudla/MetaudlaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udlaverso.metaudla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaudlaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
